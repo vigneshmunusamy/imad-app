f66e10e3e19e6a55a18c36b19d4fbcbac1075f19
vigneshmunusamy <31398150+vigneshmunusamy@users.noreply.github.com>
2017-09-15 12:01:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one',function(req,res){
-    res.send('this is article 1');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
     res.send('this is article 2');
