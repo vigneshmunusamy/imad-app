d7c00666f5cb22e2514102d0e1d85f7a1a818b5f
vigneshmunusamy <31398150+vigneshmunusamy@users.noreply.github.com>
2017-09-15 16:05:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,98 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles=
+{
+ Articleone: 
+   {
+    title:'Article 1|vignesh',
+    heading:'Article 1',
+    date:'sep 15,2017',
+    content:           `<p>
+                            created an article one link
+                        </p>
+                        <p>
+                            created an article one link
+                        </p> 
+                        <p>
+                            created an article one link
+                        </p>`
+    
+     },
+ articletwo: {
+     title:'Article2|vignesh',
+    heading:'Article 2',
+    date:"sep 15,2017",
+    content:           `<p>
+                            created an article 2 link
+                        </p>
+                        <p>
+                            created an article 2 link
+                        </p> 
+                        <p>
+                            created an article 2 link
+                        </p>`
+ },
+articlethree : {
+     title:'Article 3|vignesh',
+    heading:'Article 3',
+    date:"sep 15,2017",
+    content:           `<p>
+                            created an article 3 link
+                        </p>
+                        <p>
+                            created an article 3 link
+                        </p> 
+                        <p>
+                            created an article 3 link
+                        </p>`
+ }
+};
+function createtemplate(data)
+{
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+var htmltemplate=`
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+             <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+        <body>
+            <div class="container">
+            <div>
+                <a href='/'>get back to home page</a>
+                </div>
+                <div>
+                <a href='/article-two'>2nd page linke</a>
+                </div>
+                <div>
+                <a href='/article-three'>3rd page link</a>
+                </div>
+                <div>
+                <a href='/ui/madi.png'>cartoon</a>
+                </div>
+                <hr/>
+                <h1>
+                    ${heading}
+                    </h1>
+                    <div>
+                       ${date} 
+                    </div>
+                    <div>
+                        ${content}
+                    </div>
+                    </div>
+            </body>
+</html>`
+;
+return htmltemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -12,14 +104,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-app.get('/article-two',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articlesname',function(req,res){
+    var articlesname=req.param.articlesname;
+    res.send(createtemplate(articles(articlesname)));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
