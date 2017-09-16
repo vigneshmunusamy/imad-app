f663fc79563d25dda82f5bee8777d4d29144c980
vigneshmunusamy <31398150+vigneshmunusamy@users.noreply.github.com>
2017-09-16 14:35:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,10 @@ var element=document.getElementById('first');
 element.innerHTML='deleted';
 //perform functions with image
 var img=document.getElementById('madi');
-var marginLeft=0;
+var marginLef=0;
 function right(){
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
+    marginLef=marginLef+10;
+    img.style.marginLef=marginLef+'px';
 }
 img.onclick=function(){
 var interval=setInterval(right,100);
