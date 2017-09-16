7b2f379a43ad6bdd6aa82f16c7009c625a58e003
vigneshmunusamy <31398150+vigneshmunusamy@users.noreply.github.com>
2017-09-16 13:47:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
+//change the html text
 var element=document.getElementById('first');
 element.innerHTML='deleted';
+//perform functions with image
+var img=document.getElementById('madi');
+img.onclick=function(){
+img.style.marginLeft="100px";
+};
