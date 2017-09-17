d36631ee81e1ee66317e2abfb104b85e0f90fdfa
vigneshmunusamy <31398150+vigneshmunusamy@users.noreply.github.com>
2017-09-17 17:29:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,6 +102,15 @@ app.get('/counter', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+var names=[];
+app.get('/submit-name/:name', function (req, res) {
+  //get the parameter
+  var names=req.params.name;
+  //add the variable whenever submit button is clicked
+  names.push(names);
+  //using JSON to change array or objects to a string
+  res.send(JSON.stringify(names));
+});
 app.get('/:articlename',function(req,res){
     var articlesname=req.params.articlename;
     res.send(createtemplate(articles[articlesname]));
