890904ba857b350a26603faeaaea66557837ff4e
vigneshmunusamy <31398150+vigneshmunusamy@users.noreply.github.com>
2017-09-19 16:28:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,12 +138,14 @@ app.get('/articles/articlename',function(req,res){
         if(err){
             res.status(500).send(err.toString());
         }
-        else if(result.rows.length === 0){
+        else {
+            if(result.rows.length === 0){
             res.status(404).send('article not found');
         }
         else{
         var articlesname=result.rows[0];
         res.send(createtemplate(articlesname));
+        }
         }
     });
 });
