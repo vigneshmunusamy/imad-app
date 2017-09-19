645ca63040269c9250eb500b59ca3681dbc15a69
vigneshmunusamy <31398150+vigneshmunusamy@users.noreply.github.com>
2017-09-19 21:40:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.get('/submit-name', function (req, res) {
   res.send(JSON.stringify(names));
 });
 app.get('/articles/:articlename',function(req,res){
-    pool.query("SELECT * FROM article WHERE title ="  + req.params.articlename , function(err,result){
+    pool.query("SELECT * FROM article WHERE title ="+req.params.articlename , function(err,result){
        //if(err){
          //   res.status(500).send(err.toString());
       //  }
