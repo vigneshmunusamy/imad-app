5c18fc852b15fed08b5284bfa96f2d1d518d3607
vigneshmunusamy <31398150+vigneshmunusamy@users.noreply.github.com>
2017-09-19 17:07:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.get('/articles/:articlename',function(req,res){
             res.status(500).send(err.toString());
         }
         else {
-            if(result.rows.length == 0){
+            if(result.rows.length === 0){
             res.status(404).send('article not found');
         }
         else{
