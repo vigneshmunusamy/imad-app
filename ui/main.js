865c2142ba7428e0c9058b0802f723fe840b3016
vigneshmunusamy <31398150+vigneshmunusamy@users.noreply.github.com>
2017-09-20 20:33:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,8 +19,8 @@ submit.onclick=function(){
     };
  var username=document.getElementById('username').value;
  var password=document.getElementById('password').value;
- console.log(username);
- console.log(password);
+ //console.log(username);
+ //console.log(password);
     request.open('POST','http://vickyvijay1147.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username: username,password: password}));
