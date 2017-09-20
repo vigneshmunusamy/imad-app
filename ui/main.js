f6c3e68fb2a4be2db5b2cc6a245ce40d3836d81d
vigneshmunusamy <31398150+vigneshmunusamy@users.noreply.github.com>
2017-09-20 19:41:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,22 +9,17 @@ submit.onclick=function(){
         if(request.readystate==XMLHttpRequest.Done){
             //take some action
          if(request.status==200){
-    //make a request to the server
-    //capture and render the name list
-    var names=request.responseText;
-    names=JSON.parse(names);
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+= '<li>' + names[i] + '</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+             alert('sucess');
+         }else if(request.status==403){
+             alert('invalid');
+         }else if(request.status==500){
+             alert('unknown error');
          }
-        //not done yet
     }
     };
  var username=document.getElementById('username').value;
  var password=document.getElementById('password').value;
     request.open('POST',"http://vickyvijay1147.imad.hasura-app.io/login",true);
+    request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify(username: username,password: password));
 };
