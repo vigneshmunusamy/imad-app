6d70ad62b9e365327e0d6fa58f9f2e49b6efc6d1
vigneshmunusamy <31398150+vigneshmunusamy@users.noreply.github.com>
2017-09-22 08:41:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,5 +23,5 @@ submit.onclick=function(){
  console.log(password);
     request.open('POST','http://vickyvijay1147.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({"username": username,"password": password}));
+    request.send(JSON.stringify({username: username,password: password}));
 };
